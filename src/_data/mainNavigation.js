4c8659388f435588ba8e2d994bc46a48cb9c73ad
Ondřej Senohrábek <ondrej@MACBOOK-SENO.seno>
2026-02-26 20:09:26 +0100

    fix: header and landing fixes

--- src/_data/mainNavigation.js
+++ src/_data/mainNavigation.js
@@ -1,18 +1,18 @@
 export default {
     items: [
         { url: "/", key: "Domů" },
-        { url: "/o-nas", key: "O nás" },
+        { url: "/o-nas/", key: "O nás" },
         {
-            url: "/pro-rodice-a-zaky", key: "Pro rodiče a žáky", subitems: [
+            url: "/pro-rodice-a-zaky/", key: "Pro rodiče a žáky", subitems: [
                 { url: "/pro-rodice-a-zaky#deti-a-zaci", key: "Děti a žáci" },
                 { url: "/pro-rodice-a-zaky#rodice", key: "Rodiče" },
                 { url: "/pro-rodice-a-zaky#podle-veku-ditete", key: "Podle věku dítěte" },
                 { url: "/pro-rodice-a-zaky#dokumenty", key: "Dokumenty a materiály" }
             ]
         },
-        { url: "/pro-skoly", key: "Pro školy" },
-        { url: "/dokumenty-ke-stazeni", key: "Dokumenty" },
-        { url: "/novinky", key: "Novinky" },
-        { url: "/kontakty", key: "Kontakty" }
+        { url: "/pro-skoly/", key: "Pro školy" },
+        { url: "/dokumenty-ke-stazeni/", key: "Dokumenty" },
+        { url: "/novinky/", key: "Novinky" },
+        { url: "/kontakty/", key: "Kontakty" }
     ]
 }
